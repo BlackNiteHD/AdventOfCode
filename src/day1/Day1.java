7d0001d57a677e6eeb51bb3a636b679eb3381005
BlackNiteHD <blacknitehd@gmail.com>
2022-12-03 17:46:22 +0100

    Day1

---
+++ src/day1/Day1.java
@@ -0,0 +1,42 @@
+package day1;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Day1 {
+    public static void main(String[] args) throws IOException {
+        File file = new File(Day1.class.getResource("input.txt").getFile());
+        if(file.exists())
+        {
+            FileInputStream fr = new FileInputStream(file);
+            StringBuilder content = new StringBuilder();
+            int i;
+            while((i=fr.read())!=-1)
+            {
+                content.append((char) i);
+            }
+
+            ArrayList<Elf> elves = new ArrayList<>();
+            String[] lines = content.toString().split("\\r?\\n");
+            ArrayList<Integer> currentCaloriesForElf = new ArrayList<>();
+            for (String line : lines) {
+                if(line.equals(""))
+                {
+                    int maxCalories = 0;
+                    for (Integer calories : currentCaloriesForElf) {
+                        maxCalories+=calories;
+                    }
+                    elves.add(new Elf(currentCaloriesForElf.stream().mapToInt(Integer::intValue).toArray()));
+                    currentCaloriesForElf.clear();
+                }
+                else {
+                    currentCaloriesForElf.add(Integer.parseInt(line));
+                }
+            }
+
+            elves.sort((o1, o2) -> o2.getCaloriesSum() - o1.getCaloriesSum());
+            System.out.println("L'elf avec le plus de calories : " + elves.get(0).getCaloriesSum());
+        }
+    }
+}
---
+++ src/day1/Elf.java
@@ -0,0 +1,22 @@
+package day1;
+
+public class Elf {
+
+    private int[] calories;
+
+    public Elf(int[] calories) {
+        this.calories = calories;
+    }
+
+    public int[] getCalories() {
+        return calories;
+    }
+
+    public int getCaloriesSum() {
+        int sum = 0;
+        for (int calorie : calories) {
+            sum+=calorie;
+        }
+        return sum;
+    }
+}
